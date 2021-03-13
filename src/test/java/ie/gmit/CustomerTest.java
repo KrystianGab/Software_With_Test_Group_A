d838ce1809bbef875015abab70da3d0426ceb147
Emmett Cowan <knockanillaun@gmail.com>
2021-03-13 12:19:37 +0000

    Tests for Email, address and phone

--- src/test/java/ie/gmit/CustomerTest.java
+++ src/test/java/ie/gmit/CustomerTest.java
@@ -37,5 +37,39 @@ public class CustomerTest {
         assertEquals("invalid PPS must be 7 digits followed by a letter", e.getMessage());
     }
 
+    @Test
+    void testConstructorAddress(){
+        assertEquals(c1.getAddress() ,"97 Rhylin Glebe");
+    }
+
+    @Test
+    void testConstructorAddressFail(){
+        Exception e = assertThrows(IllegalArgumentException.class, ()-> new Customer("Emmett Cowan", "123456b", "123", "g00354127@gmit.ie", "0892297790"));
+        assertEquals("invalid Adress must be at least 8 Charaters", e.getMessage());
+    }
+
+    @Test
+    void testConstructorEmail(){
+        assertEquals(c1.getEmail() ,"g00354127@gmit.ie");
+    }
+
+    @Test
+    void testConstructorEmailFail(){
+        Exception e = assertThrows(IllegalArgumentException.class, ()-> new Customer("Emmett Cowan", "123456b", "97 Rhylin Glebe", "asdfgv", "0892297790"));
+        assertEquals("invalid Email must be at least 8 Charaters", e.getMessage());
+    }
+
+    @Test
+    void testConstructorPhone(){
+        assertEquals(c1.getPhone() ,"g00354127@gmit.ie");
+    }
+
+    @Test
+    void testConstructorPhoneFail(){
+        Exception e = assertThrows(IllegalArgumentException.class, ()-> new Customer("Emmett Cowan", "123456b", "97 Rhylin Glebe", "g00354127@gmit.ie", "12345"));
+        assertEquals("invalid Phone must be at least 7 digits", e.getMessage());
+    }
+
+
 
 }
