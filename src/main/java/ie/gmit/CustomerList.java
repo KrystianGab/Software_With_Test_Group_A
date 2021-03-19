3f8587ec7890656e8b1743687673792e75eeb9a3
Emmett Cowan <knockanillaun@gmail.com>
2021-03-19 10:31:21 +0000

    Initial Commit

---
+++ src/main/java/ie/gmit/CustomerList.java
@@ -0,0 +1,14 @@
+package ie.gmit;
+
+import java.util.ArrayList;
+
+public class CustomerList {
+
+    private ArrayList<Customer> Customers;
+
+    public  CustomerList(){
+        Customers = new ArrayList<Customer>();
+    }
+
+
+}
